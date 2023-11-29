1594fa577f26f709c7980cc593cb0a7458c93d6c
Gino Baptista <gb.g@live.com>
2023-11-29 17:06:52 -0400

    correccion del middleware jwt

--- controllers/vehiculo.controlle.js
+++ controllers/vehiculo.controlle.js
@@ -1,5 +1,8 @@
 const Persona = require('../models/persona.model')
 const Vehiculo = require('../models/vehiculos.model')
+
+const {FormData} =require('formdata-node')
+const form = new FormData()
 const cloudinary = require('cloudinary').v2
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -14,30 +17,37 @@ const vehiculos_post = async (req, res) => {
         placa,
         year_vehiculo,      
         uid_cliente } = req.body
+  
+
     try {
+        
         const unCliente = await Persona.findByPk(uid_cliente)
         const { tipo_persona } = unCliente
-        if (tipo_persona == 'cliente') {
-            if (req.files && Object.keys(req.files).length !== 0 && req.files.archivo) {
-                const { tempFilePath } = req.files.archivo
-                //const { bytes, secure_url } = await cloudinary.uploader.upload(tempFilePath, { folder: 'taller' })
-                const cloudinary = await cloudinary.uploader.upload(tempFilePath, { folder: 'taller' })
-                res.json(cloudinary)
-            } else {
-                const nuevoVehiculo = await Vehiculo.create({
-                    marca,
-                    modelo,
-                    placa,
-                    year_vehiculo,
-                    uid_cliente
-                })
-                return res.json(nuevoVehiculo)
-            }
-        } else {
-            return res.status(400).json({
-                mensaje: 'El usuario no es un cliente'
-            })
-        }
+        console.log('TIPO PERSONA: ',tipo_persona)
+        
+        
+        // if (tipo_persona == 'cliente') {
+        //     if (req.files && Object.keys(req.files).length !== 0 && req.files.archivo) {
+        //         const { tempFilePath } = req.files.archivo
+        //         //const { bytes, secure_url } = await cloudinary.uploader.upload(tempFilePath, { folder: 'taller' })
+        //         const cloudinary = await cloudinary.uploader.upload(tempFilePath, { folder: 'taller' })
+        //         res.json(cloudinary)
+        //     } else {
+        //         const nuevoVehiculo = await Vehiculo.create({
+        //             marca,
+        //             modelo,
+        //             placa,
+        //             year_vehiculo,
+        //             img_url:'',
+        //             uid_cliente
+        //         })
+        //         return res.json(nuevoVehiculo)
+        //     }
+        // } else {
+        //     return res.status(400).json({
+        //         mensaje: 'El usuario no es un cliente'
+        //     })
+        // }
 
     } catch (error) {
         console.log(error)
--- middleware/validarJWT.js
+++ middleware/validarJWT.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const Taller = require('../models/taller.model')
+const Persona = require('../models/persona.model')
 
 
 const validarJWT = async (req, res, next) => {
@@ -15,21 +16,23 @@ const validarJWT = async (req, res, next) => {
         const { uuid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY)
 
         const tallerAutenticado = await Taller.findOne({ where: { uid: uuid } })
-
-
-        if (!tallerAutenticado) {
-            return res.status(401).json({
-                mensaje: 'El usuario no existe en la base de datos'
-            })
-        }
+        const personaAutenticado = await Persona.findOne({ where: { uid: uuid } })
 
         if (tallerAutenticado) {
             req.usuarioAutenticado = tallerAutenticado
+            console.log('request: ', req.usuarioAutenticado)
+            next()
+        } else {
+            if (!personaAutenticado) {
+                return res.status(401).json({
+                    mensaje: 'El usuario no existe en la base de datos'
+                })
+            } else {
+                req.usuarioAutenticado = personaAutenticado
+                console.log('request: ', req.usuarioAutenticado)
+                next()
+            }
         }
-        
-        console.log('request: ', req.usuarioAutenticado)
-        next()
-
     } catch (error) {
         console.log(error)
         return res.status(401).json({
